f9bdd135228a591e5da40be5a86eef76034e95b5
pablomalave <pablomalave@gmail.com>
2023-01-25 20:18:42 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const API = "https://api.github.com/users/";
+
+const app = Vue.createApp({
+    data(){
+        return {
+            search: null
+        };
+    },
+    methods:{
+        async doSearch() {
+            const response = await fetch(API + this.search);
+            const data = await response.json();
+            console.log(data);
+        }
+    }
+});
+
+const mountedApp = app.mount("#app");
