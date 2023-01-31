cd5a05afab7110b1f4ab4d185f5a627371213c1e
Pablo Malave <63867180+pablomalave@users.noreply.github.com>
2023-01-31 11:40:27 -0400

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -5,17 +5,20 @@ const app = Vue.createApp({
         return {
             search: null,
             title: "Buscador de Perfiles de Github",
-            result: null
+            result: null,
+            error: null
         };
     },
     methods:{
         async doSearch() {
+          this{
             const response = await fetch(API + this.search);
             const data = await response.json();
             console.log(data);
             this.result = true;
+            }
+          }
         }
-    }
 });
 
 const mountedApp = app.mount("#app");
