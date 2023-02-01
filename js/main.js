6d53ba14afb9cadb38d2cdbb219067fba7cf92e9
Pablo Malave <63867180+pablomalave@users.noreply.github.com>
2023-02-01 14:43:03 -0400

    update js/main.js

--- js/main.js
+++ js/main.js
@@ -11,13 +11,18 @@ const app = Vue.createApp({
     }, //end data
     methods:{
         async doSearch() {
+          this.result = this.error = null;
           try {
             const response = await fetch(API + this.search);
+            console.log(response.ok);
+            if (!response.ok) throw new Error("Usuario no encontrado");
             const data = await response.json();
             console.log(data);
-            this.result = true;
+            this.result = data;
+            console.log(this.result.name);
           } catch (e) {
-            this.error = true;
+            this.error = e;
+            console.log(this.error);
           } finally {
             this.search = null;
             }
